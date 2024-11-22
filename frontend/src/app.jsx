b4eac9d967f0eaabf06ce2cadbb425dc27a2dc0d
fans656 <fans656@yahoo.com>
2024-11-22 18:13:59 +0800

    use fansjs auth

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useMemo, useEffect, useCallback } from 'react'
 import Cookies from 'js-cookie';
 import * as jose from 'jose';
-import { Routed, Layout } from 'fansjs/ui';
+import { Routed, Layout, Auth } from 'fansjs/ui';
 
 import { api } from 'src/api';
-import { UserContext } from 'src/contexts';
 import { Home } from 'src/home';
 import { Users } from 'src/users';
 
@@ -24,11 +23,10 @@ const pages = [
 ];
 
 export default function App() {
-  const user = useUser();
   return (
-    <UserContext.Provider value={user}>
-      {user.refreshed ? <Routed>{pages}</Routed> : null}
-    </UserContext.Provider>
+    <Auth>
+      <Routed>{pages}</Routed>
+    </Auth>
   );
 }
 
@@ -40,6 +38,7 @@ function Page({children}) {
           {label: 'Home', path: '/'},
           {label: 'Users', path: '/users'},
         ]}
+        auth={true}
       />
       <Layout.Content center>
         {children}
@@ -52,38 +51,4 @@ function NotFound() {
   return (
     <p>Oops! Not found</p>
   );
-}
-
-function useUser() {
-  const [data, set_data] = useState();
-  
-  const refresh = useCallback(() => {
-    const token = Cookies.get('token');
-    if (token) {
-      try {
-        const data = jose.decodeJwt(token);
-        set_data({username: data.user, refreshed: true});
-      } catch (e) {
-        console.error('user.refresh exception', e);
-        set_data({username: undefined, refreshed: true});
-      }
-    } else {
-      set_data({username: undefined, refreshed: true});
-    }
-  }, []);
-  
-  const logout = useCallback(() => {
-    Cookies.remove('token');
-    refresh();
-  }, [refresh]);
-
-  useEffect(() => {
-    refresh();
-  }, []);
-
-  const user = useMemo(() => {
-    return {...data, refresh, logout};
-  }, [data, refresh, logout]);
-  
-  return user;
 }
--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -1,60 +1,21 @@
 import React, { useState, useMemo, useEffect, useCallback } from 'react'
-import { Form, Button, message } from 'fansjs/ui';
+import { Form, Button, Auth, message } from 'fansjs/ui';
 
 import { api } from 'src/api';
-import { UserContext } from 'src/contexts';
 
 export function Home() {
-  const user = React.useContext(UserContext);
+  const user = Auth.useUser();
   if (user.username) {
-    return <Profile user={user}/>;
+    return <Page/>;
   } else {
-    return <Login user={user}/>;
+    return <Auth.Login style={{paddingTop: '2em'}}/>;
   }
 }
 
-function Profile({user}) {
+function Page() {
   return (
-    <div className="vert margin">
-      <h3>Profile</h3>
-      <p>User: {user.username}</p>
-      <Button
-        onClick={user.logout}
-      >
-        Log out
-      </Button>
+    <div className="flex-1">
+      home
     </div>
-  );
-}
-
-function Login({user}) {
-  return (
-    <Form
-      fields={[
-        {name: 'username', label: 'Username', type: 'input'},
-        {name: 'password', label: 'Password', type: 'password'},
-        {name: 'login', label: 'Login', type: 'submit'},
-      ]}
-      submit={async ({username, password}) => {
-        const res = await api.post('/api/login', {username, password}, {res: 'raw'});
-        switch (res.status) {
-          case 200:
-            message.success('Login success');
-            user.refresh();
-            break;
-          case 400:
-            message.error((await res.json()).detail);
-            break;
-          case 422:
-            message.error('Invalid input');
-            break;
-          default:
-            message.error('Unknown error');
-            console.log(res);
-            break;
-        }
-      }}
-      style={{width: '30em', marginTop: '5em', marginRight: '10em'}}
-    />
   );
 }
