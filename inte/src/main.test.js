5e4b1625d7a33fc634f45b131703d0a0c9af2921
fans656 <fans656@yahoo.com>
2024-12-01 17:59:09 +0800

    intetest

--- inte/src/main.test.js
+++ inte/src/main.test.js
@@ -1,11 +1,21 @@
-import { ports } from 'fansjs';
+import * as playwright from '@playwright/test';
 import { test, expect } from '@playwright/test';
+import { ports } from 'fansjs';
+import { Intetest } from 'fansjs/testutil';
 
 test.describe.configure({mode: 'parallel'});
-const origin = `http://localhost:${ports.auth_web}`;
+
+const inte = new Intetest({
+  playwright,
+  origin: `http://localhost:${ports.auth_web}`,
+});
 
 test.describe('login', () => {
-  test('show login form', async ({page}) => {
-    await page.goto(`${origin}`);
+  test('can show login form', async ({page}) => {
+    await inte.verify(page, '/', async ({elem}) => {
+      await elem('#username', {'type': 'input'});
+      await elem('#password', {'type': 'input'});
+      await elem('#login', {'type': 'button'});
+    });
   });
 });
