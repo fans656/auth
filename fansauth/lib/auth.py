1512df61d2fcb4d1d17b4541fe8a63ed04b0108a
fans656 <fans656@yahoo.com>
2024-12-08 02:01:04 +0800

    sugar option `login/check` in auth lib

--- fansauth/lib/auth.py
+++ fansauth/lib/auth.py
@@ -36,19 +36,58 @@ class CheckMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, req, call_next):
         req.state.get_user = lambda: _get_user(req)
         try:
-            await self.check_func(req)
-            return await call_next(req)
+            check_result = await self.check_func(req)
         except HTTPException as exc:
             return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
         except Exception as exc:
             return JSONResponse(status_code=500, content={"detail": "Internal Server Error"})
+        else:
+            if check_result is False:
+                raise HTTPException(403)
+            return await call_next(req)
 
 
 def auth(
         app,
         provider: str = 'https://auth.fans656.me',
-        check: Callable[[Request, User], bool] = None,
+        login: bool = False,
+        admin: bool = False,
+        check: Callable[[Request], bool] = None,
 ):
+    """
+    Add authorization to a FastAPI app.
+
+    Params:
+
+        app - A FastAPI app
+
+        provider: str - Authorization server URL, defaults to "https://auth.fans656.me"
+
+        login: bool - if True then requires login for all endpoint by default
+
+        admin: bool - if True then requires admin login for all endpoint by default
+
+        check: Callable[[Request], bool] - Custom authorization check function
+
+            Check function takes a FastAPI Request object as solely argument,
+            and has `request.state.get_user()` function to retrieve a optional `User` object.
+
+                def check_func(request: Request):
+                    user = request.state.get_user()
+                    if not user:
+                        if request.url.path.startswith('/api/public/'):
+                            return True
+                        else:
+                            raise HTTPException(401)
+
+            You can return False in check function to indicate HTTP 403 Forbidden response,
+            or raise custom HTTPException directly.
+            Note `None` return value will pass the authorization check.
+
+    Returns:
+
+        The FastAPI app
+    """
     if isinstance(app, FastAPI):
         app.post('/api/grant')(api_grant)
 
@@ -69,7 +108,14 @@ def auth(
         raise TypeError(f'unsupported app type {type(app)}')
 
     if check:
-        app.add_middleware(CheckMiddleware, check_func=check)
+        check_func = check
+    elif admin:
+        check_func = _check_admin_login
+    elif login:
+        check_func = _check_user_login
+
+    if check_func:
+        app.add_middleware(CheckMiddleware, check_func=check_func)
 
     return app
 
@@ -95,15 +141,6 @@ def api_grant(req: dict, response: Response):
         # max_age=token.expire_seconds,
     )
     return {'token': token}
-
-
-def _get_user(req):
-    token = req.cookies.get('token')
-    try:
-        data = jwt.decode(token, _globals['public_key'], algorithms=['RS256'])
-        return User(data)
-    except Exception:
-        return None
 
 
 def dep_User(req: Request):
@@ -132,7 +169,29 @@ def auth_check(check_func):
 auth.check = auth_check
 
 
+def _get_user(req):
+    token = req.cookies.get('token')
+    try:
+        data = jwt.decode(token, _globals['public_key'], algorithms=['RS256'])
+        return User(data)
+    except Exception:
+        return None
+
+
 def _wrap_as_async(func):
     async def wrapped(*args, **kwargs):
         return func(*args, **kwargs)
     return wrapped
+
+
+def _check_user_login(req):
+    user = _get_user(req)
+    if not user:
+        raise HTTPException(401)
+    return user
+
+
+def _check_admin_login(req):
+    user = _check_user_login(req)
+    if not user.is_admin():
+        raise HTTPException(403)
